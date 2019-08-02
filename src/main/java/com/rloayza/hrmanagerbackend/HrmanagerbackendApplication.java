2c9799075a6bbbae4471239ac30f225bdb325564
rommel <rommel.lexlara@gmail.com>
2019-08-01 22:14:13 -0400

    initial commit

---
+++ src/main/java/com/rloayza/hrmanagerbackend/HrmanagerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.rloayza.hrmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmanagerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmanagerbackendApplication.class, args);
+	}
+
+}
